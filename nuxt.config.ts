a4d615066ab0a4192adb8da98e05f9867763def4
Artyom Leonov <61983418+leonov0@users.noreply.github.com>
2025-09-10 16:42:04 +0300

    feat: setup project (#1)
    * chore: add eslint
    
    * chore: add license file
    
    * feat: add tailwind css
    
    * feat: add icons
    
    * feat: add nuxt content plugin
    
    * chore: update readme to reflect resume website template features
    
    * chore: remove unnecessary packages, install nuxt as dev dependency

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,18 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
+import tailwindcss from '@tailwindcss/vite'
+
 export default defineNuxtConfig({
+  modules: ['@nuxt/eslint', '@nuxt/icon', '@nuxt/content'],
+  devtools: { enabled: true },
+  css: ['~/assets/css/main.css'],
   compatibilityDate: '2025-07-15',
-  devtools: { enabled: true }
+  vite: {
+    plugins: [
+      tailwindcss(),
+    ],
+  },
+  eslint: {
+    config: {
+      stylistic: true,
+    },
+  },
 })
